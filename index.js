39044c36682697be8bb211ae0857e1744ecfade5
Umut Aktaş <umut@iqmotion.com>
2022-12-09 15:58:34 +0300

    added:  bsbw

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 const puppeteer = require("puppeteer-extra");
 const StealthPlugin = require("puppeteer-extra-plugin-stealth");
 const fs = require("fs");
-const BaseUrl = "https://www.nxtlvlmarine.com";
+const BaseUrl = "https://www.bsbw.com";
 const invPages = [
-  BaseUrl + "/--inventory",
-  BaseUrl + "/--inventory?pg=2",
-  BaseUrl + "/--inventory?pg=3",
-  BaseUrl + "/--inventory?pg=4",
-  BaseUrl + "/--inventory?pg=5",
-  BaseUrl + "/--inventory?pg=6",
-  BaseUrl + "/--inventory?pg=7",
-  BaseUrl + "/--inventory?pg=8",
+  BaseUrl +
+    "/boats-for-sale-in-newport-rhode-island-boat-dealer--inventory?pg=1",
+  BaseUrl +
+    "/boats-for-sale-in-newport-rhode-island-boat-dealer--inventory?pg=2",
 ];
 const allNextLevel = [];
 puppeteer.use(StealthPlugin());
@@ -49,7 +45,6 @@ puppeteer.launch({ headless: false }).then(async (browser) => {
       }
       let product = {
         specs: {},
-        manufacturerInfo: {},
       };
       const specifications = Array.from(
         document.querySelectorAll(
@@ -59,19 +54,6 @@ puppeteer.launch({ headless: false }).then(async (browser) => {
       specifications?.forEach((spec) => {
         product.specs[camelCase(spec.querySelector("label")?.innerText)] =
           spec.querySelector("span")?.innerText;
-      });
-
-      const manufacturerInfo = Array.from(
-        document.querySelectorAll(
-          "#accordionManufacturerInfo .panel-body ul .liUnit"
-        )
-      );
-      manufacturerInfo?.forEach((info) => {
-        if (info.querySelector("span")) {
-          product.manufacturerInfo[
-            camelCase(info.querySelector("label")?.innerText)
-          ] = info.querySelector("span")?.innerText;
-        }
       });
 
       product.name = document.querySelector(".unitTitle h1")?.innerText;
@@ -91,6 +73,6 @@ puppeteer.launch({ headless: false }).then(async (browser) => {
     await page.close();
   }
 
-  fs.writeFileSync("nextlevelitems.json", JSON.stringify(products));
+  fs.writeFileSync("bsbwitems.json", JSON.stringify(products));
   browser.close();
 });
